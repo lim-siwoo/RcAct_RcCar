d1048832d7aefda6da196d766bb46b01d6c717ba
ddalkytokky <tnsdyd6933@naver.com>
2024-11-22 14:16:25 +0900

    Feat: mqtt 송신 확인 ㅘㄴ료

--- soonyong/Bixby/Bixby.py
+++ soonyong/Bixby/Bixby.py
@@ -5,6 +5,9 @@ from data import get_sentence
 from text_speech import tts
 import time
 
+ip = "70.12.229.60"
+topic_name = "iot/bixby/command"
+
 def remove_whitespace_and_newlines(input_str):
     return input_str.replace(" ", "").replace("\n", "")
 
@@ -15,8 +18,8 @@ if __name__ == '__main__':
     listening_thread = threading.Thread(target=listening, args=(stop_event,))
     listening_thread.start()
 
-    mqtt_client = MQTT("127.0.0.1", 1883)
-    # mqtt_client.connect()
+    mqtt_client = MQTT(ip, 1883)
+    mqtt_client.connect()
     
     tts("안녕하세요. 저는 빅스비입니다. 무엇을 도와드릴까요?")
     try:
@@ -38,23 +41,22 @@ if __name__ == '__main__':
                 if "따라와" in sentence:
                     tts("네, 따라갈게요.")
                     called_flag = False
-                    mqtt_client.publish("command/", "follow")
+                    mqtt_client.publish(topic_name, "follow")
                 elif "돌아" in sentence:
                     tts("네, 개인기를 보여드릴게요.")
                     called_flag = False
-                    mqtt_client.publish("command/", "turn")
+                    mqtt_client.publish(topic_name, "turn")
                 elif "멈춰" in sentence:
                     tts("네, 멈출게요.")
                     called_flag = False
-                    mqtt_client.publish("command/", "stop")
+                    mqtt_client.publish(topic_name, "stop")
                 elif "목록" in sentence or "리스트" in sentence or "뭐할수있어" in sentence:
                     tts("제가 할 수 있는 일은 따라와, 돌아, 멈춰 입니다.")
                     called_flag = False
             elif called_flag:
                 tts("하고픈 일이 있다면 다시 불러주세요.")
                 called_flag = False
-                
-    tts("안녕히 가세요.")
+        tts("안녕히 가세요.")
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
         tts("안녕히 가세요.")
